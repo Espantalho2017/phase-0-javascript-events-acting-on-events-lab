1f7d18ba76a63dbadf04d957346156900248cce8
Roscoe Moore <rmoore@peersat.com>
2023-06-16 12:08:36 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+// below code creates a function to move dodger left 
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+// below code creates a function to move dodger right 
+// instead of moving the dodger ${left - 1}px, 
+// you'll be moving it ${left + 1}px.
+// using the dodger's style.right property to move the dodger right, 
+// will not because changing the style.right property 
+// doesn't change the style.left property, 
+// which means we'd have conflicting information 
+// about where the dodger should be on the screen
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
